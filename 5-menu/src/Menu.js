a2073027c9671f868b94d8c0e9ff8128f727622a
Aminat Puebla <ap2296@njit.edu>
2021-07-23 11:16:35 -0400

    intial

---
+++ 5-menu/src/Menu.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import menu from './data'
+
+const Menu = ({items}) => {
+  return (
+    <div className='section-center'>
+      {items.map((menuItem) => {
+        const {id, title, img, desc} = menuItem
+        return (
+          <article key={id} className='menu-item'>
+            <img src={img} alt={title} className='photo'/>
+          </article>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Menu
