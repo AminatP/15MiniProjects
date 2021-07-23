dd01305d31b8f6fe2cd84e28a3aab64c8133d984
Aminat Puebla <ap2296@njit.edu>
2021-07-23 11:55:19 -0400

    complete

--- 5-menu/README.md
+++ 5-menu/README.md
@@ -1,1 +1,3 @@
 Simple Menu App
+
+![image](https://user-images.githubusercontent.com/23129042/126808795-96f3838e-c86c-403e-8d79-97f84a1016ec.png)
--- 5-menu/src/Categories.js
+++ 5-menu/src/Categories.js
@@ -1,11 +1,23 @@
-import React from 'react'
 
-const Categories = () => {
+import React from 'react';
+
+const Categories = ({ categories, filterItems }) => {
   return (
-    <div>
-      <h1>Categories</h1>
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button"
+            className="filter-btn"
+            key={index}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Categories
+export default Categories;
